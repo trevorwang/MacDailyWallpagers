b402341c5a4d3829e6575a38969d634ca558835a
Trevor Wang <oumei.vip@gmail.com>
2015-05-05 14:59:43 +0800

    save pic to user home dir Pictures

--- daily_wallpapers.rb
+++ daily_wallpapers.rb
@@ -5,17 +5,19 @@ require 'appscript'  # use`gem install rb-appscript` to install appscript
 
 http_url = "http://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=zh-cn"
 
-system('mkdir -p $HOME/Pictures/wallpagers')
+`mkdir -p $HOME/Pictures/wallpapers`
+home = `echo $HOME`.strip
 
 begin
 	pic_hash = JSON.parse(Net::HTTP.get(URI(http_url)))
 	url = pic_hash["images"][0]["url"]
 	name = url.split('/').last
-	pic_file = "/tmp/#{name}" 
+	pic_file = "#{home}/Pictures/wallpapers/#{name}" 
 	File.open pic_file, 'wb' do |client|
 		open url, 'rb' do | remote|
 			client.write remote.read
 			Appscript.app("System Events").desktops.picture.set pic_file
+			puts "set wallpaper successfully!"
 		end
 	end
 rescue Exception => e
